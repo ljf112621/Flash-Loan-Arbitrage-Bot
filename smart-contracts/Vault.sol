b9f0e7c9c044664265a836f6e872329bd8183f6a
ljf112621 <33509238+ljf112621@users.noreply.github.com>
2024-06-09 18:57:42 +0700

    chore: bump dependencies to latest versions in Vault.sol

--- smart-contracts/Vault.sol
+++ smart-contracts/Vault.sol
@@ -38,3 +38,4 @@ contract EnterpriseYieldVault is ReentrancyGuard, Ownable {
 // Optimized logic batch 4922
 // Optimized logic batch 2317
 // Optimized logic batch 5112
+// Optimized logic batch 8935
